89f1c5c6f2a2684406f744dc3ebb4bd63cee26d0
ddlgdc <166570618+ddlgdc@users.noreply.github.com>
2024-09-25 22:18:25 -0700

    sessions done, all sections complete

--- flask-survey/app.py
+++ flask-survey/app.py
@@ -3,13 +3,11 @@ from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'password'
+app.config['SECRET_KEY'] = 'some_password'
 app.config['DEBUG'] = True
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 toolbar = DebugToolbarExtension(app)
-
-responces_to_survey_questions = []
 
 # home / default page
 @app.route('/')
@@ -17,44 +15,62 @@ def home():
     # Displays start of survye page with base.html
     return render_template('base.html')
 
+# new route to handle start of survey 
+@app.route('/start-survey', methods=['POST'])
+def start_survey():
+    # intialize the session responses list to store the usrs answers
+    session['responses'] = [] # new list made each user session
+    return redirect('/questions/1') # redirect to question 1
+
 # route for questions
 @app.route('/questions/<int:question_id>', methods=['POST', 'GET'])
 def question(question_id):
+    # retreive the session responses
+    responses = session.get('responses', [])
+
     # check if question_id is valid
     if question_id < 1 or question_id > len(satisfaction_survey.questions):
         flash('You are trying to access an invalid question or skip ahead!')
         return redirect('/')
     
-    # if the user has answered questions, check their progress
-    if len(responces_to_survey_questions) != question_id - 1:
-        # redirect to next quest
-        return redirect(f'/questions{len(responces_to_survey_questions) + 1}')
+    # check is user is trying to access a question out of order 
+    if len(responses) != question_id - 1:
+        flash('You are trying to skip a question.')
+        return redirect(f'/questions/{len(responses) + 1}')
     
     # handle the post req to store the answer
     if request.method == 'POST':
-        if question_id == 3: # handle the special case for question 3 (money amount)
-            money_amount = request.form.get('money')
-            if not money_amount:
-                return render_template('question_three.html', question = satisfaction_survey.questions[2], error = 'Please enter an amount.')
-            responces_to_survey_questions.append(f'${money_amount}')
+        if question_id == 3:
+            money = request.form.get('money')
+            if not money:
+                question = satisfaction_survey.questions[question_id - 1]
+                return render_template(f'question_{question_id}.html',question = question, error='Please enter a value.')
+            responses.append(money)
         else:
             answer = request.form.get('answer')
             if not answer:
-                return render_template(f'question_{question_id}.html', question = satisfaction_survey.questions[question_id - 1], error = 'Please select an answer.')
-            responces_to_survey_questions.append(answer)
+                question = satisfaction_survey.questions[question_id - 1]
+                return render_template(f'question_{question_id}.html', question = question, error = 'Please select an answer')
         
-        # redirect to the nest quewtions or to thank you page
+        # append the anser to session list 
+            responses.append(answer)
+        session['responses'] = responses
+
+        # redirect to next question or thank you page
         if question_id == len(satisfaction_survey.questions):
             return redirect('/thank_you')
         return redirect(f'/questions/{question_id + 1}')
     
+    # if its a get req, render question template
     question = satisfaction_survey.questions[question_id - 1]
     return render_template(f'question_{question_id}.html', question = question)
 
 # thank you page with responces
 @app.route('/thank_you', methods=['GET', 'POST'])
 def thank_you():
-    return render_template('thank_you.html', responses = responces_to_survey_questions)
+    # retreive responses from session 
+    responses = session.get('responses', [])
+    return render_template('thank_you.html', responses = responses)
 
 if __name__ == '__main__':
     app.run(debug = True, port = 5001)
